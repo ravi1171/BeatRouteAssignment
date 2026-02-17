18af8137cc77ab94e1af9cf8fccb21d1545610e2
Ravi Kashyap <ravikashyap1998@gmail.com>
2026-02-17 13:13:27 +0530

    code reformat

--- app/src/main/java/com/example/beatrouteassignment/data/remote/ProductRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/beatrouteassignment/data/remote/ProductRemoteDataSourceImpl.kt
@@ -1,20 +1,15 @@
 package com.example.beatrouteassignment.data.remote
 
-import com.example.beatrouteassignment.di.IoDispatcher
 import com.example.producthandling.StreamLibAPI
 import com.example.producthandling.model.Product
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ProductRemoteDataSourceImpl @Inject constructor(
     private val api: StreamLibAPI,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : ProductRemoteDataSource {
 
-    override suspend fun getAllProducts(): List<Product> = withContext(ioDispatcher) {
-        api.getAllProducts().toList()
-    }
+    override suspend fun getAllProducts(): Collection<Product> = api.getAllProducts().toList()
+
 
     override suspend fun getPriceTax(): Double = api.getPriceTax()
     override suspend fun getCompanyUpdatedPrices(): List<Pair<Int, Double>> =
